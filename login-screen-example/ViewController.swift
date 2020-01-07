ebb762e8408b817f9b285bc11f714541eec2b7ce
Farazaulia <raz.muhammad14@gmail.com>
2020-01-07 11:02:46 +0700

    Initial Commit

---
+++ login-screen-example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  login-screen-example
+//
+//  Created by Faraz Muhammad Aulia on 07/01/20.
+//  Copyright © 2020 Faraz Muhammad Aulia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
